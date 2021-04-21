039497f9f556642b85a58a1cadd2ca1073f82631
jagtapnn0509 <jagtapnn05@gmail.com>
2021-04-21 11:23:38 +0530

    Add a function to get Result of Second Computation program

--- ArithmeticComputation.sh
+++ ArithmeticComputation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 echo "Welcome to Sorting Arithmetic Computation Problem"
-
+# input take from user
 read -p "Enter First Number :" a
 read -p "Enter Second Number :" b
 read -p "Enter Third Number :" c
