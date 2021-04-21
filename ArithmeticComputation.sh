98699676059c604c54cb17abf2cc2be8f31c4e42
jagtapnn0509 <jagtapnn05@gmail.com>
2021-04-21 11:13:03 +0530

    Add a function to get result of First Computation Program

--- ArithmeticComputation.sh
+++ ArithmeticComputation.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -x
 echo "Welcome to Sorting Arithmetic Computation Problem"
 
-read -p "Enter First Number" a
-read -p "Enter Second Number" b
-read -p "Enter Third Number" c
+read -p "Enter First Number :" a
+read -p "Enter Second Number :" b
+read -p "Enter Third Number :" c
+
+result1=`awk "BEGIN {print $a + $b * $c}"`
+echo " Result of First Computation Program is : $result1"
