d330118d02418e82b375ee052f1a9ad829164111
jagtapnn0509 <jagtapnn05@gmail.com>
2021-04-21 11:19:18 +0530

    Add a function to get result of First Computation Program

--- ArithmeticComputation.sh
+++ ArithmeticComputation.sh
@@ -7,3 +7,6 @@ read -p "Enter Third Number :" c
 
 result1=`awk "BEGIN {print $a + $b * $c}"`
 echo " Result of First Computation Program is : $result1"
+
+result2=`awk "BEGIN {print $a * $b + $c}"`
+echo " Result of Second Computation Program is : $result2"
